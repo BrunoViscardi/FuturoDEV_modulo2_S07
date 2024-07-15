009a9fb4f9e510833f25d4bc19695d32e6f56405
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-07-15 17:21:11 -0300

    feat: criação da rota post para cadastrar um curso

---
+++ src/controllers/CursoController.js
@@ -0,0 +1,24 @@
+const Curso = require("../models/Curso")
+
+
+class CursoController {
+
+    
+
+    async criar(request, response) {
+        try {
+            const dados = request.body
+            /* validacao */
+            const curso = await Curso.create(dados)
+            response.status(201).json(curso)
+        } catch (error) {
+            response.status(500).json({
+                mensagem: 'Houve um erro ao cadastrar o curso', error
+            })
+        }
+    }
+
+    
+}
+
+module.exports = new CursoController()
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express')
 
 
+const cursosRoutes = require('./cursos.routes')
+
+
 const routes = new Router()
 
 
+routes.use('/cursos', cursosRoutes)
 
 
 module.exports = routes
