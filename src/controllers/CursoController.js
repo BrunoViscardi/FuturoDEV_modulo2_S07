3a879b6f3dd221dbea0625ac8bc4cf42ecfdb900
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-07-16 12:19:05 -0300

    feat: implementa validações a rota post criar curso

--- src/controllers/CursoController.js
+++ src/controllers/CursoController.js
@@ -8,7 +8,21 @@ class CursoController {
     async criar(request, response) {
         try {
             const dados = request.body
-            /* validacao */
+
+            //Validação: Colunas obrigatórias
+            if (!dados.nome || !dados.duracao) {
+                return response.status(400).json({
+                    mensagem: 'O Nome do curso e sua Duração são obrigatórios '
+                })
+            }
+
+            //Validação: Duração é um numero inteiro
+            if (!Number.isInteger(dados.duracao)) {
+                return response.status(400).json({
+                    mensagem: 'A Duração do curso deve ser um número inteiro'
+                })
+            }
+            
             const curso = await Curso.create(dados)
             response.status(201).json(curso)
         } catch (error) {
